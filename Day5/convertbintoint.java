35e64f5a8a943d7e2d28af59baaa954534042613
Ayushigupta7453 <133493980+Ayushigupta7453@users.noreply.github.com>
2024-07-21 23:34:55 +0530

    day5 started

---
+++ Day5/convertbintoint.java
@@ -0,0 +1,12 @@
+public class convertbintoint {
+    public int getDecimalValue(ListNode head) {
+        StringBuilder sb=new StringBuilder();
+       ListNode temp=head;
+       while(temp!=null){
+           sb.append(temp.val);
+           temp=temp.next;
+       }
+       int decimal = Integer.parseInt(sb.toString(), 2);
+       return decimal;
+   }   
+}
